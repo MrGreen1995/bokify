c0c48b90d20d8f3d2112c8c160c12dde1ce0e215
o.mardonov <o.mardonov@rtsb.uz>
2025-05-29 12:55:52 +0500

    Domain Events added

--- src/Bookify.Domain/Abstractions/Entity.cs
+++ src/Bookify.Domain/Abstractions/Entity.cs
@@ -2,10 +2,27 @@
 
 public abstract class Entity
 {
+    private readonly List<IDomainEvent> _domainEvents = new();
+    
     public Guid Id { get; init; }
 
     public Entity(Guid id)
     {
         Id = id;
+    }
+    
+    public IReadOnlyList<IDomainEvent> GetDomainEvents()
+    {
+        return _domainEvents.ToList();
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
     }
 }
