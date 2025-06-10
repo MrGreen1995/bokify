b7bb11580309bf8d447f4def0d45b84dc3269251
o.mardonov <o.mardonov@rtsb.uz>
2025-06-10 18:50:08 +0500

    Finish User Registration

---
+++ src/Bookify.Application/Users/RegisterUser/RegisterUserCommand.cs
@@ -0,0 +1,7 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+
+namespace Bookify.Application.Users.RegisterUser
+{
+    public sealed record RegisterUserCommand(string Email, string FirstName, string LastName, string Password) : ICommand<Guid>;
+    
+}
