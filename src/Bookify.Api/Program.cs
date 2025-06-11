f5305021aa2f84c55f86c1c7dfee3929582252fd
o.mardonov <o.mardonov@rtsb.uz>
2025-06-11 19:17:10 +0500

    Add serilog

--- src/Bookify.Api/Program.cs
+++ src/Bookify.Api/Program.cs
@@ -1,8 +1,11 @@
 using Bookify.Api.Extensions;
 using Bookify.Application;
 using Bookify.Infrastructure;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -24,6 +27,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseSerilogRequestLogging();
 
 app.UseCustomExceptionHandler();
 
