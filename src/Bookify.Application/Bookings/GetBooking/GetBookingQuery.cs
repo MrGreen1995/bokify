d48f567dc7e168f1641a30281029fde14bcae460
o.mardonov <o.mardonov@rtsb.uz>
2025-06-12 19:48:01 +0500

    Redis Distributed caching added

--- src/Bookify.Application/Bookings/GetBooking/GetBookingQuery.cs
+++ src/Bookify.Application/Bookings/GetBooking/GetBookingQuery.cs
@@ -1,6 +1,10 @@
-﻿using Bookify.Application.Abstractions.Messaging;
-using Bookify.Domain.Bookings;
+﻿using Bookify.Application.Abstractions.Caching;
 
 namespace Bookify.Application.Bookings.GetBooking;
 
-public sealed record GetBookingQuery(Guid BookingId) : IQuery<BookingResponse>;
+public sealed record GetBookingQuery(Guid BookingId) : ICachedQuery<BookingResponse>
+{
+    public string CacheKey => $"bookings-{BookingId}";
+
+    public TimeSpan? Expiration => null;
+}
