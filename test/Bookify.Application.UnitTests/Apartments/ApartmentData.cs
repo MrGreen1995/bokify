dc8c4f0a51a440e91d68471c3f3fde6cb27de15c
o.mardonov <o.mardonov@rtsb.uz>
2025-06-16 17:49:36 +0500

    Application Unit Tests Added

---
+++ test/Bookify.Application.UnitTests/Apartments/ApartmentData.cs
@@ -0,0 +1,17 @@
+﻿using Bookify.Domain.Apartments;
+using Bookify.Domain.Shared;
+
+namespace Bookify.Application.UnitTests.Apartments
+{
+    internal static class ApartmentData
+    {
+        public static Apartment Create() => new(
+            Guid.NewGuid(),
+            new Name("Test apartment name"),
+            new Description("Test description"),
+            new Address("Country", "State", "ZipCode", "City", "Street"),
+            new Domain.Shared.Money(100.0m, Currency.Usd),
+            Money.Zero(),
+            []);
+    }
+}
---
+++ test/Bookify.Application.UnitTests/Users/UserData.cs
@@ -0,0 +1,13 @@
+﻿using Bookify.Domain.Users;
+
+namespace Bookify.Application.UnitTests.Users
+{
+    internal static class UserData
+    {
+        public static User Create() => User.Create(FirstName, LastName, Email);
+
+        public static readonly FirstName FirstName= new("FirstName");
+        public static readonly LastName LastName= new("LastName");
+        public static readonly Email Email= new("Email");
+    }
+}
